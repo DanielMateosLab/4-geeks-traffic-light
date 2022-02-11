a0099974388f356f6a34c7c9c65740baf5ce6934
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-11 21:45:56 +0000

    Add first looping functionality

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,8 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Light from "./Light.jsx";
 
-const TrafficLight = ({ colors }) => {
+const TrafficLight = ({ colors, looping }) => {
 	const [activeColor, setActive] = useState();
+	// TODO: clean code, implement loop checkbox
+	const [intervalId, setIntervalId] = useState();
+
+	const [count, setCount] = useState(0);
+	function addCount() {
+		setCount((prevCount) =>
+			prevCount < colors.length - 1 ? prevCount + 1 : 0
+		);
+	}
+	useEffect(() => {
+		setActive(colors[count]);
+	}, [count]);
+
+	function removePreviousInterval() {
+		console.log(`Cleaning ` + intervalId);
+		clearInterval(intervalId);
+	}
+
+	useEffect(() => {
+		intervalId && removePreviousInterval();
+
+		const id = setInterval(addCount, 1000);
+		console.log(`Interval set with id ${id}`);
+		setIntervalId(id);
+	}, [looping, colors]);
 
 	return (
 		<div className="tl-box">
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "./Nav.jsx";
 
 import TrafficLight from "./TrafficLight.jsx";
 
 const Home = () => {
 	const [colors, setColors] = useState(["red", "orange", "limegreen"]);
+	const [looping, setLooping] = useState(true);
 
 	return (
 		<div className="root">
-			<Nav setColors={setColors} />
-			<TrafficLight colors={colors} />
+			<Nav setColors={setColors} setLooping={setLooping} />
+			<TrafficLight colors={colors} looping={looping} />
 		</div>
 	);
 };
