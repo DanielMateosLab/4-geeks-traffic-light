ceb2824319cc019b63fe5567e903c7855449f2e2
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-12 18:30:33 +0000

    Add lightbulb and clean light-glow code

--- src/js/component/Light.jsx
+++ src/js/component/Light.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
 const Light = ({ color, active, setActive }) => {
-	const onBoxShadow = `0 0 4rem 1rem ${color}`;
-	const offBoxShadow = "inset 0 0 4rem black";
+	const activeBoxShadow = `0 0 4rem 1rem ${color}`;
 
 	return (
 		<div
-			className="light"
+			className={`light ${active ? "active" : ""}`}
 			style={{
 				background: color,
-				boxShadow: active ? onBoxShadow : offBoxShadow,
 			}}
-			onClick={() => setActive(color)}
-		/>
+			onClick={() => setActive(color)}>
+			<div
+				className="light-glow"
+				style={active ? { boxShadow: activeBoxShadow } : {}}
+			/>
+			<div className="lightbulb" />
+		</div>
 	);
 };
 
